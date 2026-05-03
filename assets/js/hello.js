1b79537d03e41238a8e68f5a3d23fcb194518d44
whw00 <aprdot00@snu.ac.kr>
2026-05-03 20:51:33 +0000

    과제 최종 수정_05:51

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,7 +1,7 @@
-const name = "우혜원";
+const studentname = "우혜원";
 const major = "사회과학대학";
 const siteYear = 2026;
 
-const greeting = '안녕하세요! 저는 ${name}입니다. 이 사이트는 ${siteYear}년에 만든 ${major} 학생의 개인 웹사이트입니다.';
+const greeting = `안녕하세요! 저는 ${studentname}입니다. 이 사이트는 ${siteYear}년에 만든 ${major} 학생의 개인 웹사이트입니다.`;
 
 console.log(greeting);
