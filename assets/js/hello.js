6d2a4220ac80e9745eae45726ccf318f662e1606
whw00 <aprdot00@snu.ac.kr>
2026-05-03 20:45:52 +0000

    과제 최종 수정_05:45

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,5 +1,5 @@
 const name = "우혜원";
-const name = "사회과학대학";
+const major = "사회과학대학";
 const siteYear = 2026;
 
 const greeting = '안녕하세요! 저는 ${name}입니다. 이 사이트는 ${siteYear}년에 만든 ${major} 학생의 개인 웹사이트입니다.';
